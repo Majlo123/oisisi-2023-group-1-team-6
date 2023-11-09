95b05a7250b10dabc31544ef8f5a44208ab7af49
Mihajlo Bogdanovic <bogdanovicmihajlo68@gmail.com>
2023-11-09 04:18:02 +0100

    Update Student.cs

--- CLI/Model/Student.cs
+++ CLI/Model/Student.cs
@@ -3,10 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using StudentskaSluzba.Serialization;
+namespace StudentskaSluzba.Model;
 
-namespace CLI.Model
+class Student : ISerializable
 {
-    internal class Student
+    public string Surname { get; set; }
+
+    public string Name { get; set; }
+
+    public string Date {  get; set; }
+
+    //public Address Address { get; set; }
+
+    public long PhoneNumber {  get; set; }
+
+    public string Email { get; set; }
+    
+    public int Id { get; set; }
+
+    public int YearoOfStudy {  get; set; }
+
+    public enum Status { B, S};
+
+    public float AvarageGrade { get; set; }
+
+    public List<Subject> Subjects { get; set; }
+
+    public Student()
     {
+        Subjects = new List<Subject>();
+
+    }
+   
+
+
+
+
+
+
+
+
+
+
+
+    public string[] ToCSV()
+    {
+        string[] csvValues =
+        {
+            Id.ToString(),
+            Name
+        };
+        return csvValues;
+    }
+
+    public void FromCSV(string[] values)
+    {
+        Id = int.Parse(values[0]);
+        Name = values[1];
     }
 }
