d2d28e86edb02efbd7177047b1e2b57e7e53b283
Mihajlo Bogdanovic <bogdanovicmihajlo68@gmail.com>
2023-11-10 03:59:58 +0100

    Students DAO done

--- GUI/DAO/StudentsDAO.cs
+++ GUI/DAO/StudentsDAO.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using StudentskaSluzba.Model;
 using StudentskaSluzba.Storage;
 
@@ -27,6 +28,39 @@ namespace GUI.DAO
             return _students[^1].Id;
 
         }
+        private Student? GetStudentById(int id)
+        {
+            return _students.Find(s => s.Id == id);
+        }
+        public Student addStudent(Student student)
+        {
+            student.Id = GenerateId(); 
+            _students.Add(student);
+            _storage.Save(_students);
+            return student;
+        }
+        public Student? UpdateStudent(Student student)
+        {
+            Student? oldStudent = GetStudentById(student.Id);
+            if(oldStudent == null) { return null; }
+            oldStudent.Surname = student.Surname;
+            oldStudent.Name= student.Name;
+            oldStudent.Date= student.Date;
+            oldStudent.Address= student.Address;
+            oldStudent.PhoneNumber= student.PhoneNumber;
+            oldStudent.Email= student.Email;
+            oldStudent.YearOfStudy= student.YearOfStudy;
+            oldStudent.Status= student.Status; 
+            oldStudent.AvarageGrade= student.AvarageGrade;
+            oldStudent.PassedSubjects= student.PassedSubjects;
+            oldStudent.FailedSubjects= student.FailedSubjects;
 
+            return student;
+
+        }
+        public List<Student> GetAllStudents()
+        {
+            return _students;
+        }
     }
 }
